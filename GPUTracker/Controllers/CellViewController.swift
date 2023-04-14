0769bcdf2b3b9692140381a147e2fbfb66a823d0
TomBolton <goralexwizard@gmail.com>
2023-04-14 20:14:02 +0300

    add cellVC

---
+++ GPUTracker/Controllers/CellViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CellViewController.swift
+//  GPUTracker
+//
+//  Created by Alexander on 14.04.2023.
+//
+
+import UIKit
+
+class CellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
