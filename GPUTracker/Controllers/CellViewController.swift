f036cc71f545fb43300722d4ca65b4ac74efcfdf
TomBolton <goralexwizard@gmail.com>
2023-04-14 20:20:13 +0300

    improvements

--- GPUTracker/Controllers/CellViewController.swift
+++ GPUTracker/Controllers/CellViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class CellViewController: UIViewController {
+    let contacts = Source.generateVendors()
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupTableView()
+    }
+    
+}
+
+extension CellViewController {
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
     
 }
