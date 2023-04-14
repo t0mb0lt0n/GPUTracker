29686e7b1ad165b711b9e28b0a3e4acb559b0318
TomBolton <goralexwizard@gmail.com>
2023-04-14 21:16:47 +0300

    cell cfg

--- GPUTracker/Controllers/MainViewController.swift
+++ GPUTracker/Controllers/MainViewController.swift
@@ -20,6 +20,7 @@ class MainViewController: UIViewController {
         tableView.register(VendorCellView.self, forCellReuseIdentifier: "VendorCell")
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.isScrollEnabled = false
     }
     
 }
@@ -47,10 +48,10 @@ extension MainViewController {
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 500),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            tableView.topAnchor.constraint(equalTo: mainView!.processorsLabel.bottomAnchor, constant: 10),
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.heightAnchor.constraint(equalToConstant: 119)
         ])
     }
     
