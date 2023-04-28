6b1265dd2b636340fc07f2972dedefa8092bbac1
TomBolton <goralexwizard@gmail.com>
2023-04-28 18:34:41 +0300

    core data CFG

--- GPUTracker/Models/CoreDataManager.swift
+++ GPUTracker/Models/CoreDataManager.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
-
+import CoreData
 
 class CoreDataManager: UICollectionViewCell {
+    static let shared = CoreDataManager()
+    private init() {}
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var context: NSManagedObjectContext = {
+        NSPersistentContainer.viewContext
+    }()
 }
