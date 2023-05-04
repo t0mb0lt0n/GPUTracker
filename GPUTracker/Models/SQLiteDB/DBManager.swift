2e3e5dd6942272d56888de7a2428cc71f493164b
TomBolton <goralexwizard@gmail.com>
2023-05-04 19:15:26 +0300

    some improvements

--- GPUTracker/Models/SQLiteDB/DBManager.swift
+++ GPUTracker/Models/SQLiteDB/DBManager.swift
@@ -20,42 +20,43 @@ func addGPU(vendor: String, id: Int, description: String) {
         let vendorField = Expression<String>("vendor")
         let descriptionField = Expression<String>("description")
         let idField = Expression<Int>("id")
-        try db.run(amd.insert(vendorField <- vendor))
-        try db.run(amd.insert(descriptionField <- description))
-        try db.run(amd.insert(idField <- id))
+        try db.run(amd.update(vendorField <- vendor))
+        try db.run(amd.update(descriptionField <- description))
+        try db.run(amd.update(idField <- id))
+        print(try db.prepare(amd))
     }
     catch {
         print(error.localizedDescription)
     }
 }
 
-func getFromDB() -> String {
-    var result = ""
-    do {
-        let path = NSSearchPathForDirectoriesInDomains(
-            .documentDirectory, .userDomainMask, true
-        ).first!
-        _ = copyDatabaseIfNeeded(sourcePath: Bundle.main.path(forResource: "gpuDB", ofType: "db")!)
-
-        let db = try Connection("\(path)/gpuDB.db")
-        let gpu = Table("gpu")
-        let vendor = Expression<String>("vendor")
-        let description = Expression<String>("description")
-        var result = ""
-        
-        //достать из базы
-        for someVendor in try db.prepare(gpu.filter(vendor == "amd")) {
-            //print("id: \(someVendor[vendor])")
-            print(someVendor)
-            //try someVendor.get(Expression<String>("vendor")).where { $0 == "AMD"}
-        }
-        
+func getFromDB()  {
+//    var result = ""
+//    do {
+//        let path = NSSearchPathForDirectoriesInDomains(
+//            .documentDirectory, .userDomainMask, true
+//        ).first!
+//        _ = copyDatabaseIfNeeded(sourcePath: Bundle.main.path(forResource: "gpuDB", ofType: "db")!)
+//
+//        let db = try Connection("\(path)/gpuDB.db")
+//        let gpu = Table("AMD")
+//        let vendor = Expression<String>("vendor")
+//        let description = Expression<String>("description")
+//        var result = ""
+//
+//        //достать из базы
+//        for someVendor in try db.prepare(gpu.filter(vendor == "amd")) {
+//            //print("id: \(someVendor[vendor])")
+//            print(someVendor)
+//            //try someVendor.get(Expression<String>("vendor")).where { $0 == "AMD"}
+//      }
+//
+//
+//    }
+//    catch {
+//        print(error.localizedDescription)
+//    }
     
-    }
-    catch {
-        print(error.localizedDescription)
-    }
-    return result
 }
 
 
