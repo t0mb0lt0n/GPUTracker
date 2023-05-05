f4b002181fd79c00170f89130ee405b0cfe08a19
TomBolton <goralexwizard@gmail.com>
2023-05-05 19:53:28 +0300

    some improvements

--- GPUTracker/Models/SQLiteDB/DBManager.swift
+++ GPUTracker/Models/SQLiteDB/DBManager.swift
@@ -20,8 +20,12 @@ func addGPU(vendor: String, id: Int, description: String) {
         let vendorField = Expression<String>("vendor")
         let descriptionField = Expression<String>("description")
         let idField = Expression<Int>("id")
-        try db.run(amd.update(vendorField <- vendor))
-       
+        //try db.run(amd.insert(vendorField <- vendor, idField <- id, descriptionField <- description))
+        try db.run(amd.insertMany(or: OnConflict.ignore, [vendorField <- "amd2"], [descriptionField <- "geoff@mac.com"], [idField <- 100]))
+        //try db.run(amd.where(vendorField == "amd").update(descriptionField <- "some description"))
+       // try db.run(amd.where(vendorField == "100").update(descriptionField <- "100"))
+        //try db.run(amd.update(idField <- id))
+        //try db.run(amd.update(descriptionField <- description))
         
     }
     catch {
@@ -29,38 +33,38 @@ func addGPU(vendor: String, id: Int, description: String) {
     }
     
     
-    
+   
     
     
     
 }
 
 func getFromDB()  {
-//    var result = ""
-//    do {
-//        let path = NSSearchPathForDirectoriesInDomains(
-//            .documentDirectory, .userDomainMask, true
-//        ).first!
-//        _ = copyDatabaseIfNeeded(sourcePath: Bundle.main.path(forResource: "gpuDB", ofType: "db")!)
-//
-//        let db = try Connection("\(path)/gpuDB.db")
-//        let gpu = Table("AMD")
-//        let vendor = Expression<String>("vendor")
-//        let description = Expression<String>("description")
-//        var result = ""
-//
-//        //достать из базы
-//        for someVendor in try db.prepare(gpu.filter(vendor == "amd")) {
-//            //print("id: \(someVendor[vendor])")
-//            print(someVendor)
-//            //try someVendor.get(Expression<String>("vendor")).where { $0 == "AMD"}
-//      }
-//
-//
-//    }
-//    catch {
-//        print(error.localizedDescription)
-//    }
+    var result = ""
+    do {
+        let path = NSSearchPathForDirectoriesInDomains(
+            .documentDirectory, .userDomainMask, true
+        ).first!
+        _ = copyDatabaseIfNeeded(sourcePath: Bundle.main.path(forResource: "gpuDB", ofType: "db")!)
+
+        let db = try Connection("\(path)/gpuDB.db")
+        let gpu = Table("AMD")
+        let vendor = Expression<String>("vendor")
+        let description = Expression<String>("description")
+        var result = ""
+
+        //get data
+        for someVendor in try db.prepare(gpu.filter(vendor == "amd")) {
+            //print("id: \(someVendor[vendor])")
+            print(someVendor)
+           try someVendor.get(Expression<String>("vendor")).where { $0 == "AMD"}
+      }
+
+
+    }
+    catch {
+        print(error.localizedDescription)
+    }
     
 }
 
