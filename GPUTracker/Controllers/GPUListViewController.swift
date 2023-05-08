46056d5b1e75ee29859512757d11adfe24d809ed
TomBolton <goralexwizard@gmail.com>
2023-05-08 17:55:27 +0300

    cell cfg

---
+++ GPUTracker/Controllers/GPUListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GPUListViewController.swift
+//  GPUTracker
+//
+//  Created by Alexander on 08.05.2023.
+//
+
+import UIKit
+
+class GPUListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
