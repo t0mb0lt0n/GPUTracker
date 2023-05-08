2519828d706761ebb3c17e80ea5e3b06f232eb47
TomBolton <goralexwizard@gmail.com>
2023-05-08 17:44:25 +0300

    cell cfg

--- GPUTracker/Views/GPUInfoCellView.swift
+++ GPUTracker/Views/GPUInfoCellView.swift
@@ -8,16 +8,48 @@
 import UIKit
 
 class GPUInfoCellView: UITableViewCell {
+    let cardImage = UIImageView()
+    
+    let cardName: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    let rightArrowImage: UIImageView = {
+        let image = UIImageView()
+        let arrowImage = UIImage(systemName: "chevron.right")?.withTintColor(.systemGray, renderingMode: .alwaysOriginal)
+        image.image = arrowImage
+        return image
+    }()
+    
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+        setupConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupCell() {
+        [cardImage, cardName, descriptionLabel, rightArrowImage].forEach { subView in
+            subView.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(subView)
+        }
+    }
+    
+    private func setupConstraints() {
+        
+    }
+    
 }
