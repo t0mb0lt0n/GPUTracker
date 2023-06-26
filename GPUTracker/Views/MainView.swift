b06d6a7a21cef3ef5d43a39b0fd191adbc1eed60
TomBolton <goralexwizard@gmail.com>
2023-06-26 21:35:21 +0300

    improvements

--- GPUTracker/Views/MainView.swift
+++ GPUTracker/Views/MainView.swift
@@ -30,7 +30,7 @@ class MainView: UIView {
     
     let appNameLabel: UILabel = {
         let label = UILabel()
-        label.text = "GPUTracker v0.9.0.1"
+        label.text = "GPUTracker v0.9.0.3"
         label.textColor = .systemGray
         label.font = .systemFont(ofSize: 15, weight: .light)
         label.textAlignment = .center
