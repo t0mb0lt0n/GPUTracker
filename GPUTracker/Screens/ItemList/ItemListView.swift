f6e2d0a711e35c6b44a57a8d4410143ad5272659
TomBolton <goralexwizard@gmail.com>
2024-04-04 16:04:06 +0300

    itemListVC

---
+++ GPUTracker/Screens/ItemList/ItemListView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListView.swift
+//  GPUTracker
+//
+//  Created by Alexander on 04.04.2024.
+//
+
+import UIKit
+
+class ItemListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GPUTracker/Screens/ItemList/ItemListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListViewController.swift
+//  GPUTracker
+//
+//  Created by Alexander on 04.04.2024.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
