6ba45c29b7655c0303f6d3d76f822619fee71150
TomBolton <goralexwizard@gmail.com>
2024-04-05 18:06:48 +0300

    page controll

--- GPUTracker/CustomViews/CustomDescriptionView.swift
+++ GPUTracker/CustomViews/CustomDescriptionView.swift
@@ -15,12 +15,45 @@ final class CustomDescriptionView: UIView {
         return label
     }()
     
+    let testScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .lightGray
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.bounces = true
+        scrollView.contentSize = CGSize(width: Int(UIScreen.main.bounds.width) - 20, height: 1000)
+        var testLable1: UILabel = {
+            let label = UILabel()
+            label.text = "Test label text"
+            label.backgroundColor = .green
+            return label
+        }()
+        scrollView.addSubview(testLable1)
+        scrollView.isPagingEnabled = true
+        return scrollView
+    }()
+    
+    func addLabel(position: CGFloat) {
+        let label = UILabel()
+        label.text = "test text"
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        testScrollView.addSubview(label)
+        let screeWith = UIScreen.main.bounds.width
+        label.frame = CGRect(x: screeWith * position, y: 0, width: screeWith, height: 200)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         backgroundColor = .gray
         setupView()
         setupConstraints()
-        
+        addLabel(position: 0)
+        addLabel(position: 1)
+        addLabel(position: 2)
+
+        addLabel(position: 3)
+
+
     }
     
     required init?(coder: NSCoder) {
@@ -30,6 +63,7 @@ final class CustomDescriptionView: UIView {
     final private func setupView() {
         [
         testLable1,
+        testScrollView,
         ].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             addSubview($0)
@@ -41,6 +75,14 @@ final class CustomDescriptionView: UIView {
             testLable1.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
             testLable1.widthAnchor.constraint(equalToConstant: 300),
             testLable1.heightAnchor.constraint(equalToConstant: 50),
+            
+            testScrollView.topAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.topAnchor,
+                constant: 0
+            ),
+            testScrollView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            testScrollView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            testScrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
     }
     
