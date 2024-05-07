63f48caae7037744c66251723222983c5b8c8ecf
TomBolton <goralexwizard@gmail.com>
2024-05-07 22:07:19 +0300

    delete empty file

--- GPUTracker/Services/Realm/RealmFiles.swift
+++ GPUTracker/Services/Realm/RealmFiles.swift
@@ -5,11 +5,4 @@
 //  Created by Alexander on 15.04.2024.
 //
 
-import Foundation
 
-struct RealmFiles {
-    let realmList: [Int: String] = [
-        0: .xbox360Realm,
-        1: .sonyRealm
-    ]
-}
