3361b18d98db5f7c7f3775981aecdbdc6bc45f1f
TomBolton <goralexwizard@gmail.com>
2024-08-16 13:46:19 +0300

    some imorovements

--- GPUTracker/CustomViews/CustomTableView.swift
+++ GPUTracker/CustomViews/CustomTableView.swift
@@ -4,21 +4,14 @@
 //
 //  Created by Alexander on 08.04.2024.
 //
-
 import UIKit
 
 class CustomTableView: UITableView {
     init() {
         if #available(iOS 15, *) {
-            super .init(
-                frame: .zero,
-                style: .insetGrouped
-            )
+            super .init(frame: .zero, style: .insetGrouped)
         } else {
-            super .init(
-                frame: .zero,
-                style: .grouped
-            )
+            super .init(frame: .zero, style: .grouped)
         }
         setupTableView()
     }
