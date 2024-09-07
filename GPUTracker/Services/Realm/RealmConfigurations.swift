d89214c35d6801de87eaf2f5bc9869b6e5f20a66
TomBolton <goralexwizard@gmail.com>
2024-09-07 13:30:45 +0300

    sch recfg

--- GPUTracker/Services/Realm/RealmConfigurations.swift
+++ GPUTracker/Services/Realm/RealmConfigurations.swift
@@ -5,18 +5,18 @@
 //  Created by Alexander on 15.04.2024.
 //
 
-//import Foundation
-//
-//struct RealmConfigurations {
-//    static let itemIndexName: [[String]] = [
-//        [.xbox360Realm,
-//        .sonyRealm],
-//        [.xbox360Realm,
-//        .sonyRealm]
-//    ]
-//    static let schemas: [String: UInt64] = [
-//        .xbox360Realm: 68,
-//        .sonyRealm: 3,
-//        .mainProductListRealm: 9
-//    ]
-//}
+import Foundation
+
+struct RealmConfigurations {
+    static let itemIndexName: [[String]] = [
+        [.xbox360Realm,
+        .sonyRealm],
+        [.xbox360Realm,
+        .sonyRealm]
+    ]
+    static let schemas: [String: UInt64] = [
+        .xbox360Realm: 68,
+        .sonyRealm: 3,
+        .mainProductListRealm: 9
+    ]
+}
