ac8f716350a1b96e3b9e26dd1ea2afc6f5b4d136
TomBolton <goralexwizard@gmail.com>
2024-09-10 18:43:24 +0300

    impr

--- GPUTracker/Helpers/Extensions/String+Extension.swift
+++ GPUTracker/Helpers/Extensions/String+Extension.swift
@@ -3,19 +3,19 @@
 //  GPUTracker
 //
 //  Created by Alexander on 04.04.2024.
+////
 //
-
-extension String {
-    static let microsoftHeader: String = "Microsoft"
-    static let segaHeader: String = "Sega"
-    static let sonyHeader: String = "Sony"
-    static let generalSegmentHeader: String = "General"
-    static let motherboardsSegmentHeader: String = "Motherboards"
-    static let otherComponentsSegmentHeader: String = "Other Components"
-    static let xbox360Realm: String = "xbox360"
-    static let sonyRealm: String = "sony"
-    static let mainProductListRealm: String = "mainProductList"
-    static let failurePlaceholder: String = "failure"
-    static let productDetailsHeader: String = "Product details"
-    static let mainCatalogue: String = "Main catalogue"
-}
+//extension String {
+//    static let microsoftHeader: String = "Microsoft"
+//    static let segaHeader: String = "Sega"
+//    static let sonyHeader: String = "Sony"
+//    static let generalSegmentHeader: String = "General"
+//    static let motherboardsSegmentHeader: String = "Motherboards"
+//    static let otherComponentsSegmentHeader: String = "Other Components"
+//    static let xbox360Realm: String = "xbox360"
+//    static let sonyRealm: String = "sony"
+//    static let mainProductListRealm: String = "mainProductList"
+//    static let failurePlaceholder: String = "failure"
+//    static let productDetailsHeader: String = "Product details"
+//    static let mainCatalogue: String = "Main catalogue"
+//}
