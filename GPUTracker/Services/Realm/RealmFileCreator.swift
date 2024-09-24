c4b49893f20f01ffa67efbb9dd526e1962074c86
TomBolton <goralexwizard@gmail.com>
2024-09-24 17:38:59 +0300

    impr

--- GPUTracker/Services/Realm/RealmFileCreator.swift
+++ GPUTracker/Services/Realm/RealmFileCreator.swift
@@ -5,17 +5,17 @@
 //  Created by Alexander on 01.04.2024.
 //
 
-import RealmSwift
-
-class RealmFileCreator {
-    var emptyRealm = try! Realm()
-    
-    func createEmptyRealmFile() {
-        ////Uncomment if required
-        //let testRecord = SonyProductList(value: AnyObject.self)
-        try! emptyRealm.write {
-            //emptyRealm.add(testRecord)
-            //emptyRealm.deleteAll()
-        }
-    }
-}
+//import RealmSwift
+//
+//class RealmFileCreator {
+//    var emptyRealm = try! Realm()
+//    
+//    func createEmptyRealmFile() {
+//        ////Uncomment if required
+//        //let testRecord = SonyProductList(value: AnyObject.self)
+//        try! emptyRealm.write {
+//            //emptyRealm.add(testRecord)
+//            //emptyRealm.deleteAll()
+//        }
+//    }
+//}
