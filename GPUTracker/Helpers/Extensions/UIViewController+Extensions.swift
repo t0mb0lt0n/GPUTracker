910de42011fe886b1e8351c48739c26569530c3f
TomBolton <goralexwizard@gmail.com>
2024-11-01 16:45:54 +0300

    Nav bar configuration

--- GPUTracker/Helpers/Extensions/UIViewController+Extensions.swift
+++ GPUTracker/Helpers/Extensions/UIViewController+Extensions.swift
@@ -4,4 +4,26 @@
 //
 //  Created by Alexander on 01.11.2024.
 //
+import UIKit
 
+extension UIViewController {
+    final func setupNavigationBarStyle(
+        isLarge: Bool,
+        title: String?,
+        titleColor: UIColor?
+    ) {
+        navigationController?.navigationBar.prefersLargeTitles = isLarge
+        guard
+            let title,
+            let titleColor
+        else
+        { return }
+        self.title = title
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: titleColor
+        ]
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: titleColor
+        ]
+    }
+}
