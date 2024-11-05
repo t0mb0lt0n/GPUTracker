0eb5af72428f0f15217b2f3071e4ea12c44ddef7
TomBolton <goralexwizard@gmail.com>
2024-11-05 21:57:24 +0300

    realm improvements

--- GPUTracker/Services/Realm/RealmObjects.swift
+++ GPUTracker/Services/Realm/RealmObjects.swift
@@ -11,7 +11,7 @@ import RealmSwift
 class General: Object {
     @objc dynamic var _id: Int = 0
     @objc dynamic var descriptionName: String = ""
-    @objc dynamic var value: String = ""
+    @objc dynamic var descriptionValue: String = ""
 
     override static func primaryKey() -> String? {
         return "_id"
