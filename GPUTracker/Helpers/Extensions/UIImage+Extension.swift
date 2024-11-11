2ff27e60a0ff502d9ae4b83bbe09f6bf4bfdbd31
TomBolton <goralexwizard@gmail.com>
2024-11-11 22:49:57 +0300

    some fix

--- GPUTracker/Helpers/Extensions/UIImage+Extension.swift
+++ GPUTracker/Helpers/Extensions/UIImage+Extension.swift
@@ -10,8 +10,8 @@ import UIKit
 extension UIImage {
     static let onlineStatusImage = UIImage(named: "onlineStatusImage") ?? UIImage()
     static let offlineStatusImage = UIImage(named: "offlineStatusImage") ?? UIImage()
-    static let partiallyAvailableStatusImage = UIImage(named: "offlineStatusImage") ?? UIImage()
+    static let partiallyAvailableStatusImage = UIImage(named: "offlineStatusImag") ?? UIImage()
     static let generalImage = UIImage(systemName: "building.columns") ?? UIImage()
-    static let consoleComponentsImage = UIImage(systemName: "cpu")?.withTintColor(.red, renderingMode: .alwaysOriginal) ?? UIImage()
+    static let consoleComponentsImage = UIImage(systemName: "scroll.fill") ?? UIImage()
     static let motherboardComponentsImage = UIImage(systemName: "wrench.and.screwdriver.fill") ?? UIImage()
 }
