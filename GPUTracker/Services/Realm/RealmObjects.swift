6d6b8cd1f7a11034d7738c1d58119c34b5a3d55f
TomBolton <goralexwizard@gmail.com>
2024-11-12 23:28:28 +0300

    realm objects updata

--- GPUTracker/Services/Realm/RealmObjects.swift
+++ GPUTracker/Services/Realm/RealmObjects.swift
@@ -48,31 +48,6 @@ class MotherboardComponents: Object {
     }
 }
 
-
-
-
-
-//class BoardRevision: Object {
-//    @objc dynamic var _id: Int = 0
-//    @objc dynamic var boardName: String = ""
-//    @objc dynamic var value: String = ""
-//
-//    override static func primaryKey() -> String? {
-//        return "_id"
-//    }
-//}
-
-//class Boards: Object {
-//    @objc dynamic var _id: Int = 0
-//    @objc dynamic var revison: String = ""
-//    @objc dynamic var hdmi: Bool = false
-//    @objc dynamic var boardName: String = ""
-//
-//    override static func primaryKey() -> String? {
-//        return "_id"
-//    }
-//}
-
 class MicrosoftProductList: Object {
     @objc dynamic var _id: Int = 0
     @objc dynamic var productName: String = ""
@@ -88,6 +63,7 @@ class SonyProductList: Object {
     @objc dynamic var _id: Int = 0
     @objc dynamic var productName: String = ""
     @objc dynamic var shortDetails: String = ""
+    @objc dynamic var onlineStatus: String = ""
     
     override static func primaryKey() -> String? {
         return "_id"
@@ -98,6 +74,7 @@ class SegaProductList: Object {
     @objc dynamic var _id: Int = 0
     @objc dynamic var productName: String = ""
     @objc dynamic var shortDetails: String = ""
+    @objc dynamic var onlineStatus: String = ""
     
     override static func primaryKey() -> String? {
         return "_id"
