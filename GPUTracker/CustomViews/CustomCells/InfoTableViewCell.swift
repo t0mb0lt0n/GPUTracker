27f0a0fe412ebb3c8e1fb319fdd5633d4d5abca7
TomBolton <goralexwizard@gmail.com>
2024-12-18 16:01:20 +0300

    InfoTableViewCell

---
+++ GPUTracker/CustomViews/CustomCells/InfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InfoTableViewCell.swift
+//  GPUTracker
+//
+//  Created by Alexander on 18.12.2024.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
